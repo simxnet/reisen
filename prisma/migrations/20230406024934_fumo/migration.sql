052cdd3758fe146fcefa1ab13775cf1037d2e855
ddiablou <jekem13936@jo6s.com>
2023-04-06 14:40:42 +0200

    feat(prisma): change to mysql

--- prisma/migrations/20230406024934_fumo/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Fumo" (
-    "id" SERIAL NOT NULL,
-    "source" TEXT NOT NULL,
-    "caption" TEXT DEFAULT 'fumofu?',
-
-    CONSTRAINT "Fumo_pkey" PRIMARY KEY ("id")
-);
